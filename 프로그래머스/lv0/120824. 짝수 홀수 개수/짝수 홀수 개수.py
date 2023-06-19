97b0e453e22d7c03dbdcaba226435dbfc752b4ab
H Lee <95900411+07LEE@users.noreply.github.com>
2023-06-19 23:53:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,5 +1,8 @@
 def solution(num_list):
-    a = len([i for i in num_list if i%2 == 0])
-    b = len(num_list) - a
-    answer = [a, b]
+    answer = [0, 0]
+    for i in num_list:
+        if i%2 == 0:
+            answer[0] += 1
+        else:
+            answer[1] += 1    
     return answer
