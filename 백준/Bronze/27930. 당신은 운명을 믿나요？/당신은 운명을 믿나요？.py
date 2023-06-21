d376477e671b56a879a0dc563f76ac6e29a1714a
H Lee <95900411+07LEE@users.noreply.github.com>
2023-06-22 01:39:21 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 44 ms, Memory: 31744 KB -BaekjoonHub

--- 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
@@ -1,17 +1,18 @@
+from sys import stdin
+input = stdin.readline
+
 s = input()
 k, y = 'KOREA', 'YONSEI'
-
 kn, yn = 0, 0
 
 for i in s:
     if i == k[kn]:
         kn += 1
+        if kn == 5:
+            print(k)
+            break
     elif i == y[yn]:
-        yn += 1
-    
-    if kn == 5:
-        print(k)
-        break
-    if yn == 6:
-        print(y)
-        break
+        yn += 1    
+        if yn == 6:
+            print(y)
+            break
