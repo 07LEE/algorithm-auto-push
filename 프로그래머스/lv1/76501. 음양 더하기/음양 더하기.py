73be3450ba2ecc84861d2822948421bfe281fd2c
H Lee <95900411+07LEE@users.noreply.github.com>
2023-07-09 15:57:06 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,8 @@
 def solution(absolutes, signs):
-    answer = sum(absolutes[i] if signs[i] == True else (absolutes[i] * -1) for i in range(len(absolutes)))
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += absolutes[i]         
+        else:
+            answer -= absolutes[i]
     return answer
