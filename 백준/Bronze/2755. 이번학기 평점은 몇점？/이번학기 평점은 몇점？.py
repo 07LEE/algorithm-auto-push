fffc247d04efe04babcb248fc8b265af0bde2182
H Lee <95900411+07LEE@users.noreply.github.com>
2023-08-01 09:32:01 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,11 @@
+score = {'A+' : 4.3, 'A0': 4.0, 'A-': 3.7, 'B+': 3.3, 'B0': 3.0, 'B-': 2.7, 'C+': 2.3, 'C0': 2.0, 'C-': 1.7, 'D+': 1.3, 'D0': 1.0, 'D-': 0.7, 'F': 0.0}
+n = int(input())
+ans = 0
+t = 0
+
+for _ in range(n):
+    a, b, c = input().split()
+    ans += score[c] * int(b)
+    t += int(b)
+
+print('{0:.2f}'.format(round(ans/t + 10**-10, 2)))
