3121e69ca4e45b22ddbeda5f2937c2b61337f15a
Orçun Lee <yzz0728@korea.ac.kr>
2024-06-17 04:00:56 +0900

    [Silver III] Title: Parity Constraint Closest Pair (Easy), Time: 300 ms, Memory: 110780 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.py
+++ 백준/Silver/27931. Parity Constraint Closest Pair （Easy）/Parity Constraint Closest Pair （Easy）.py
@@ -1,23 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def insertSort(array):
-    for i in range(1, len(array)):
-        j = i
-        while j > 0 and array[j] < array[j-1]:
-            array[j], array[j-1] = array[j-1], array[j]
-            j -= 1
-    return array
-
 n = int(input())
 m = list(map(int, input().split()))
-insertSort(m)
 odd = -1
 even = -1
 
 for i in range(n-1):
     for j in range(i+1, n):
-        k = m[j] - m[i]
+        k = abs(m[j] - m[i])
         if k%2 == 0:
             if even == -1:
                 even = k
