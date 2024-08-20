8000082ea140bea039b34eedf1279f9f88bbc5df
Lee <yzz0728@korea.ac.kr>
2024-08-21 07:39:57 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,11 @@
+n = int(input())
+for _ in range(n):
+    print('@@@@@' * n)
+for _ in range(n):
+    print('@' * n)
+for _ in range(n):
+    print('@@@@@' * n)
+for _ in range(n):
+    print('@' * n)
+for _ in range(n):
+    print('@@@@@' * n)
